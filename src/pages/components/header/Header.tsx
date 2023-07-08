d37aa9aa67018d0083a60558f4bd6cfc5afa4255
Gabryel Soares <gabryelfhsoares@gmail.com>
2023-07-08 14:16:38 -0300

    add homepage link into header logo

--- src/pages/components/header/Header.tsx
+++ src/pages/components/header/Header.tsx
@@ -5,14 +5,15 @@ import cartIcon from "../../../images/cartIcon.png";
 import { SlLocationPin } from "react-icons/sl";
 import { HiOutlineSearch } from "react-icons/hi";
 import { BiCaretDown } from "react-icons/bi";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <div className="w-full h-20 bg-amazon_blue text-white text-lightText sticky top-0 z-50">
       <div className="h-full w-full mx-auto inline-flex items-center justify-between gap-1 mdl: gap-3 px-4 ">
-        <div className="px-2 border border-transparent hover:border-white cursor-pointer diration-300 flex items-center justify-center h-[70%]">
+        <Link href="/" className="px-2 border border-transparent hover:border-white cursor-pointer diration-300 flex items-center justify-center h-[70%]">
           <Image className="w-28 object-cover mt-1" src={logo} alt="logo" />
-        </div>
+        </Link>
         <div className="px-2 border border-transparent hover:border-white cursor-pointer diration-300 flex items-center justify-center h-[70%] hidden xl:inline-flex gap-1">
           <SlLocationPin />
           <div className="text-xs">
@@ -50,7 +51,9 @@ export default function Header() {
             alt="cart image"
           />
           <p className="text-xs text-white font-bold mt-3">Cart</p>
-          <span className="absolute text-amazon_yellow text-sm top-2 left-[29px] font-semibold">0</span>
+          <span className="absolute text-amazon_yellow text-sm top-2 left-[29px] font-semibold">
+            0
+          </span>
         </div>
       </div>
     </div>
