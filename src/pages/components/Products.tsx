575ce90f12ab5771a0a3bd528e75d7372a39b911
Gabryel Soares <gabryelfhsoares@gmail.com>
2023-07-08 16:20:07 -0300

    add product cards footer

--- src/pages/components/Products.tsx
+++ src/pages/components/Products.tsx
@@ -46,11 +46,29 @@ export default function Products({ productData }: Props) {
               </div>
               {isNew && (
                 <p className="absolute top-0 right-0 text-amazon_blue font-medium text-xs tracking-wide animate-bounce">
-                  !save <FormattedPrice amount={oldPrice - price}/>
+                  !save <FormattedPrice amount={oldPrice - price} />
                 </p>
               )}
             </div>
             <hr />
+            <div className="px-4 py-3 flex flex-col gap-1">
+              <p className="text-xs text-gray-500 tracking-wide">{category}</p>
+              <p className="text-base font-medium">{title}</p>
+              <p className="flex items-center gap-2">
+                <span className="text-sm line-through">
+                  <FormattedPrice amount={oldPrice} />
+                </span>
+                <span className="text-amazon_blue font-semibold">
+                  <FormattedPrice amount={price} />
+                </span>
+              </p>
+              <p className="text-xs text-gray-600 text-justify">
+                {description.substring(0, 120)}
+              </p>
+              <button className="h-10 font-medium bg-amazon_blue text-white rounded-md hover:bg-amazon_yellow hover:text-black duration-300 mt-2">
+                add to cart
+              </button>
+            </div>
           </div>
         )
       )}
