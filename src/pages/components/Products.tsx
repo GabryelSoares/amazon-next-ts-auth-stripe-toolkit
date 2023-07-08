f2fa18bd3588c219c2ee7a07dcd265ad23f80561
Gabryel Soares <gabryelfhsoares@gmail.com>
2023-07-08 16:05:34 -0300

    add Products Page

---
+++ src/pages/components/Products.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { ProductProps } from "../../../type";
+import Image from "next/image";
+import { HiShoppingCart } from "react-icons/hi";
+import { FaHeart } from "react-icons/fa";
+
+interface Props {
+  productData: ProductProps[];
+}
+
+export default function Products({ productData }: Props) {
+  return (
+    <div className="w-full px-6 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
+      {productData.map(
+        ({
+          brand,
+          category,
+          description,
+          image,
+          isNew,
+          oldPrice,
+          price,
+          title,
+          _id,
+        }) => (
+          <div
+            key={_id}
+            className="w-full bg-white text-black p-4 border border-gray-300 rounded-lg group overflow-hidden"
+          >
+            <div className="w-full h-[260px] relative">
+              <Image
+                className="w-full h-full object-cover scale-90 hover:scale-100 transition-transform duration-300"
+                height={300}
+                width={300}
+                src={image}
+                alt="product image"
+              />
+              <div className="w-12 h-24 absolute bottom-10 right-0 border-[1px] border-gray-400 bg-white rounded-md flex flex-col translate-x-20 group-hover:translate-x-0 transition-transform duration-300">
+                <span className="w-full h-full border-b-[1px] border-b-gray-400 flex items-center justify-center text-xl bg-transparent hover:bg-amazon_yellow cursor-pointer duration-300">
+                  <HiShoppingCart />
+                </span>
+                <span className="w-full h-full border-b-[1px] border-b-gray-400 flex items-center justify-center text-xl bg-transparent hover:bg-amazon_yellow cursor-pointer duration-300">
+                  <FaHeart />
+                </span>
+              </div>
+              {isNew && (
+                <p className="absolute top-0 right-0 text-amazon_blue font-medium text-xs tracking-wide animate-bounce">
+                  !save {oldPrice - price}
+                </p>
+              )}
+            </div>
+            <hr />
+          </div>
+        )
+      )}
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,31 @@
+import { ProductProps } from "../../type";
 import Banner from "./components/Banner";
+import Products from "./components/Products";
 
-export default function Home() {
+interface Props {
+  productData: ProductProps[];
+}
+
+export default function Home({ productData }: Props) {
   return (
     <main>
       <div className="max-w-screen-2xl mx-auto">
         <Banner />
+        <div className="relative md:-mt-20 lgl:-mt-32 xl:-mt-60 z-20 mb-10">
+          <Products productData={productData} />
+        </div>
       </div>
     </main>
   );
 }
+
+// SSR for data fetching
+export const getServerSideProps = async () => {
+  const res = await fetch("https://fakestoreapiserver.reactbd.com/tech");
+  const productData = await res.json();
+  return {
+    props: {
+      productData,
+    },
+  };
+};
