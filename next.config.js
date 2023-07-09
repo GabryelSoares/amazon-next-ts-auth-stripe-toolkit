33b700449ff405ed09ad671fc886471f1cb83711
Gabryel Soares <gabryelfhsoares@gmail.com>
2023-07-09 18:49:53 -0300

    add cors origins

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  cors: {
+    origin: [
+      "http://localhost:3000/",
+      "https://amazon-next-ts-auth-stripe-toolkit-kypclfodt-gabryelsoares.vercel.app/",
+      "https://amazon-next-ts-auth-stripe-toolkit.vercel.app/",
+    ],
+    credentials: true,
+  },
   reactStrictMode: true,
   images: {
     domains: [
